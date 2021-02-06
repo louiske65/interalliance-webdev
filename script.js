1b8fd40be363901f5eeccd7667adb65cbfa2c775
louiske65 <38638922+louiske65@users.noreply.github.com>
2021-02-06 08:51:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+const keyParam = "&appid=" + key;
+const key = "YOUR OWN API KEY HERE";
+const units = "&units=imperial";
+
+// Send GET request to server and retrive relevant data
+function getWeather() {
+    const input = document.querySelector("input").value;
+    const endpoint = url + input + keyParam + units;
+    console.log(endpoint);
+    fetch(endpoint).then((response) => {
+        if (response.ok) {
+            console.log("success");
+            return response.json();
+        }
+    }).then((jsonResponse) => {
+        formatJson(jsonResponse);
+    });
+}
+
+// HTML elements in which the results will be displayed
+const description = document.getElementById("description");
+const temp = document.getElementById("temp");
+const pressure = document.getElementById("pressure");
+const humidity = document.getElementById("humidity");
+
+// Update the page with the retrieved data
+function formatJson(jsonResponse) {
+    description.innerHTML = `<p>Description: ${jsonResponse.weather[0].description.toUpperCase()}</p>`;
+    temp.innerHTML = `<p>Temperature: ${jsonResponse.main.temp} F</p>`;
+    pressure.innerHTML = `<p>Pressure: ${jsonResponse.main.pressure} hPa</p>`;
+    humidity.innerHTML = `<p>Humidity: ${jsonResponse.main.humidity}%</p>`;
+}
